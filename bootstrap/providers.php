9537b1efc88b89f2aa8b1e8e02a8f7834e2fcb86
Duran Can YILMAZ <duran4@rengaver.com>
2024-07-11 00:40:25 +0300

    remove service provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     Spatie\Permission\PermissionServiceProvider::class,
-    Dcyilmaz\LivewireDataTable\LivewireDataTableServiceProvider::class,
+
 ];

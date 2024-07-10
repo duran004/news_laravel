45138949d2fd2504a347e39150bd8258fa4d2f37
Duran Can YILMAZ <duran4@rengaver.com>
2024-07-11 00:15:14 +0300

    Class Dcyilmaz\LivewireDataTable\LivewireDataTableServiceProvider not found

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    Spatie\Permission\PermissionServiceProvider::class
+    Spatie\Permission\PermissionServiceProvider::class,
+    Dcyilmaz\LivewireDataTable\LivewireDataTableServiceProvider::class,
 ];
